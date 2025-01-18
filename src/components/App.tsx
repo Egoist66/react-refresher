6ffbcca3523b419a8b39e7a001596e90dd579c99
Farid Mahmudov <Razormad666@gmail.com>
2025-01-19 01:57:47 +0300

    update

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,13 +2,21 @@ import { Layout } from './layout/Layout'
 
 function App() {
 
+
+  const LayoutStructure = {
+    header: <h1>Header</h1>,
+    footer: <h2>Footer</h2>,
+    _default: (data: any) => <h2>{data?.layout}</h2>
+  }
+
   return (
-    <Layout
-     header={<h1>Header</h1>}
-     footer={() => <h2>Footer</h2>}
-     _default={(data) => <h2>{data?.layout}</h2>}
-    >
-    </Layout>
+   <>
+   
+    <Layout {...LayoutStructure}/>
+  
+   
+   </>
+    
   )
 }
 
